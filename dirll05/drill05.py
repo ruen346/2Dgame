dfaa762bc8ce2d7abd09fbe35ea0b89e2d71b4b5
Ruen_note <ruen346@gmail.com>
2018-09-18 18:09:03 +0900

    drill05 위로 올라감
    위로 올라가는 코드 완성

--- dirll05/drill05.py
+++ dirll05/drill05.py
@@ -15,7 +15,13 @@ def move_from_center_to_right():
         delay(0.01)
 
 def move_up():
-    pass
+    x, y = 800 - 25, 90
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
 
 def move_left():
     pass
@@ -27,7 +33,7 @@ def move_left_to_center():
     pass
 
 def make_rectangle():
-    move_from_center_to_right()
+    # move_from_center_to_right()
     move_up()
     move_left()
     move_down()
