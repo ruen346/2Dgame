6045c5ad95fd91de39200f88cfc43d8af78115d5
Ruen_note <ruen346@gmail.com>
2018-10-02 18:50:24 +0900

    drill07 생성
    생성합니다

---
+++ drill07/drill07.py
@@ -0,0 +1,35 @@
+from pico2d import *
+import random
+
+open_canvas(1280,1024)
+
+character = load_image('animation_sheet.png')
+
+hide_cursor()
+
+running = True
+frame = 0
+ch_x = 0
+ch_y = 0
+
+
+def go_character(p1,p2):
+    pass
+
+
+while running:
+
+    clear_canvas()
+
+    if (go_x > 0):
+        character.clip_draw(frame * 100, 100, 100, 100, ch_x - 25, ch_y + 25)
+    else:
+        character.clip_draw(frame * 100, 0, 100, 100, ch_x - 25, ch_y + 25)
+    go_character(p1, p2)
+
+    update_canvas()
+    get_events()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
+close_canvas()
