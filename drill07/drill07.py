93864e08c7526a123349ba5335db47b792e73326
Ruen_note <ruen346@gmail.com>
2018-10-02 19:16:51 +0900

    2017184002구건모 drill07 완료 커밋 4번
    https://www.youtube.com/watch?v=I8Y--OR4DMc&feature=youtu.be

--- drill07/drill07.py
+++ drill07/drill07.py
@@ -13,36 +13,45 @@ ch_x = 0
 ch_y = 0
 
 
-def move_character(x,y):
-    pass
+def move_character(x2,y2):
+    global ch_x
+    global ch_y
 
+    ch_x = x2
+    ch_y = y2
 
 def go_character(p1,p2):
-    for i in range(0, 100 + 1, 5):
+    global frame
+    global ch_x
+    global ch_y
+    for i in range(0, 100 + 1, 10):
         t = i / 100
-        x = (1-t)*p1[0]+t*p2[0]
-        y = (1-t)*p1[1]+t*p2[1]
-        move_character((x,y))
+        x2 = (1-t)*p1[0]+t*p2[0]
+        y2 = (1-t)*p1[1]+t*p2[1]
+
+        move_character(x2,y2)
+
+        clear_canvas()
+        if (ch_x > i):
+            character.clip_draw(frame * 100, 100, 100, 100, ch_x - 25, ch_y + 25)
+        else:
+            character.clip_draw(frame * 100, 0, 100, 100, ch_x - 25, ch_y + 25)
+        update_canvas()
+        frame = (frame + 1) % 8
         delay(0.05)
 
+
 size = 20
-points = [(random.randint(-500,500), random.randint(-350, 350)) for i in range(size)]
+points = [(random.randint(0,1000), random.randint(0, 800)) for i in range(size)]
 n = 1
+
+ch_x,ch_y = points[n-1]
 
 while running:
 
-    clear_canvas()
-
-    if (ch_x > points[n - 1]):
-        character.clip_draw(frame * 100, 100, 100, 100, ch_x - 25, ch_y + 25)
-    else:
-        character.clip_draw(frame * 100, 0, 100, 100, ch_x - 25, ch_y + 25)
+    i,j = points[n]
 
     go_character(points[n - 1], points[n])
     n = (n + 1) % size
-
-    update_canvas()
-    get_events()
-    frame = (frame + 1) % 8
 
 close_canvas()
