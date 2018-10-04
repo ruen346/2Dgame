3fc24f75b2d40e62037c58529895afe3fd5fea2f
Ruen_note <ruen346@gmail.com>
2018-10-04 18:32:50 +0900

    drill08-2 시작

---
+++ drill08/drill08_2.py
@@ -0,0 +1,24 @@
+from pico2d import *
+import random
+
+open_canvas(1280,1024)
+
+character = load_image('animation_sheet.png')
+kpu = load_image('KPU_GROUND.png')
+
+hide_cursor()
+
+running = True
+frame = 0
+ch_x = 0
+ch_y = 0
+fine = 0
+
+
+def go_character(p1,p2,p3,p4):
+    pass
+
+
+
+
+close_canvas()
