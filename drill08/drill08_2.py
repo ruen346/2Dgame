20e6b5347fecdcbfc314a7dd575b7cbe8cfaa817
Ruen_note <ruen346@gmail.com>
2018-10-04 18:33:21 +0900

    drill08-2 이동

--- drill08/drill08_2.py
+++ drill08/drill08_2.py
@@ -16,7 +16,22 @@ fine = 0
 
 
 def go_character(p1,p2,p3,p4):
-    pass
+    global frame
+    global ch_x
+    global ch_y
+    global fine
+    global points
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x2 = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y2 = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+
+        ch_x = x2
+        ch_y = y2
+
+
+
 
 
 
