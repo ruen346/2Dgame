e7bcaf04a08073d41d17ee9b0f69e5950e2c3efe
Ruen_note <ruen346@gmail.com>
2018-10-09 18:36:31 +0900

    drill09 키입력
    정의

--- drill09/drill09.py
+++ drill09/drill09.py
@@ -39,7 +39,13 @@ class Ball:
 
 
 def handle_events():
-
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 # initialization code
 open_canvas()
