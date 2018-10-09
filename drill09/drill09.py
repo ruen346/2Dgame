7b66d310d0361b5197d0075f1636827cb3ecf65a
Ruen_note <ruen346@gmail.com>
2018-10-09 18:35:18 +0900

    drill09 시작
    ㄱ

---
+++ drill09/drill09.py
@@ -0,0 +1,28 @@
+from pico2d import *
+import random
+
+# Game object class here
+class Grass:
+
+
+class Boy:
+
+
+
+class Ball:
+
+
+
+def handle_events():
+
+
+# initialization code
+open_canvas()
+
+
+# game main loop code
+while running:
+
+
+# finalization code
+close_canvas()
