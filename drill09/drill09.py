5ebb3f0ca3999cba98088296d9d79bb67cb5db45
Ruen_note <ruen346@gmail.com>
2018-10-09 18:35:54 +0900

    drill09 클레스 정의
    3클레스 정의

--- drill09/drill09.py
+++ drill09/drill09.py
@@ -3,14 +3,39 @@ import random
 
 # Game object class here
 class Grass:
-
+    def __init__(self):
+        self.image = load_image('grass.png')
+    def draw(self):
+        self.image.draw(400, 30)
 
 class Boy:
-
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 90
+        self.frame = random.randint(0, 7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 
 class Ball:
-
+    def __init__(self):
+        self.x, self.y = random.randint(50, 750), 599
+        e = random.randint(0,1)
+        if e == 0:
+            self.image = load_image('ball21x21.png')
+        else:
+            self.image = load_image('ball41x41.png')
+        self.speed = random.randint(3,7)
+    def update(self):
+        if(self.y <= 70):
+            self.y = 70
+        else:
+            self.y -= self.speed
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 
 def handle_events():
