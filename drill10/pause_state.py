8c19eb9b63386b83894591da290fdc96a0618f82
Ruen_note <ruen346@gmail.com>
2018-10-11 19:06:17 +0900

    drill10 event

--- drill10/pause_state.py
+++ drill10/pause_state.py
@@ -42,7 +42,13 @@ def exit():
 
 
 def handle_events():
-
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT: game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
 
 
 def update():
