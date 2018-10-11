97c0c15f7d93684047d097969ab611dff10dcc2e
Ruen_note <ruen346@gmail.com>
2018-10-11 19:05:00 +0900

    drill10시작

---
+++ drill10/pause_state.py
@@ -0,0 +1,39 @@
+import random
+import json
+import os
+
+from pico2d import *
+
+import game_framework
+import title_state
+import main_state
+
+
+
+name = "PauseState"
+
+pauses = None
+font = None
+
+
+class Pause:
+
+
+
+def enter():
+
+
+
+def exit():
+
+
+
+def handle_events():
+
+
+
+def update():
+
+
+
+def draw():
