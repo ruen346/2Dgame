5a32f6d018a2419cea6fe64e547c997a912748d3
Ruen_note <ruen346@gmail.com>
2018-11-22 18:22:46 +0900

    -

--- drill14/boy.py
+++ drill14/boy.py
@@ -139,7 +139,7 @@ class Boy:
         self.eat_num = 0
 
     def get_bb(self):
-        return 400 - 50, 300 - 50, 400 + 50, 300 + 50
+
 
 
     def set_background(self, bg):
@@ -160,7 +160,7 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.canvas_width//2 - 50, self.canvas_height//2 + 50, '(%5d num)' % (self.eat_num), (255, 255, 0))
+
         draw_rectangle(*self.get_bb())
 
     def eat(self, ball):
